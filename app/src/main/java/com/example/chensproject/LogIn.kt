5981becc45e5183507e86a5abcad59853b13ac26
chen <chensasson12@gmail.com>
2024-12-19 16:25:47 +0200

    15.12

--- app/src/main/java/com/example/chensproject/LogIn.kt
+++ app/src/main/java/com/example/chensproject/LogIn.kt
@@ -1,33 +1,59 @@
 package com.example.chensproject
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Phone
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
-class LogIn : AppCompatActivity() {
-    lateinit var userName: EditText;
-    lateinit var phone: EditText;
+class Login : AppCompatActivity() {
+
     lateinit var login: Button;
     lateinit var register: TextView;
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
         enableEdgeToEdge()
         setContentView(R.layout.log_in)
-        userName = findViewById(R.id.username)
-        phone = findViewById(R.id.phone)
+
         login = findViewById(R.id.loginButton)
-        //login.setOnClickListener()
         register = findViewById(R.id.register)
         login.setOnClickListener({
-            Toast.makeText(this,"hi+${userName.text}", Toast.LENGTH_SHORT).show()
+            var Email = findViewById<EditText?>(R.id.Email).text
+            var LogInPassword = findViewById<EditText?>(R.id.LogInPassword).text
+            auth.signInWithEmailAndPassword(Email.toString(), LogInPassword.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+            Toast.makeText(this,"hi+${Email.toString()}", Toast.LENGTH_SHORT).show()
     })
+
+
 }
+
+
 }
--- app/src/main/java/com/example/chensproject/MainActivity.kt
+++ app/src/main/java/com/example/chensproject/MainActivity.kt
@@ -1,36 +1,73 @@
 package com.example.chensproject
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
-    lateinit var userName:EditText;
-    lateinit var phone:EditText;
+
     lateinit var register:Button;
     lateinit var login:TextView;
+    private lateinit var auth: FirebaseAuth
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
+
+        fun reload() {
+            TODO("Not yet implemented")
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        userName = findViewById(R.id.username)
-        phone = findViewById(R.id.phone)
         register = findViewById(R.id.registerButton)
         login = findViewById(R.id.login)
         register.setOnClickListener({
-            Toast.makeText(this,"hi+${userName.text}",Toast.LENGTH_SHORT).show()
-            val intent = Intent(this,homescreen::class.java)
+            var Email = findViewById<EditText?>(R.id.Email).text
+            var SignUpPassword = findViewById<EditText?>(R.id.SignUpPassword).text
+            auth.createUserWithEmailAndPassword(Email.toString(), SignUpPassword.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        val intent = Intent(this,homescreen::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+            Toast.makeText(this,"hi+${Email.toString()}",Toast.LENGTH_SHORT).show()
+
+        })
+        login.setOnClickListener({
+            val intent = Intent(this,Login::class.java)
             startActivity(intent)
         })
+
+
+
+
+
     }
+
 }
